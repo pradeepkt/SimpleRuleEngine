de8839ab25e03a0d3ec164a87053633181cc9c25
PradeepKumarArasan <pradeepkumar.arasan@target.com>
2017-09-11 19:44:10 +0530

    Basic Interface

---
+++ src/com/simpleruleengine/BasicRuleEngine.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.simpleruleengine;
+
+/**
+ * @author z0027pb
+ *
+ */
+public interface BasicRuleEngine {
+	
+	public boolean compare(Object val1, Object val2, int operation);
+
+}
